03e22b28983eb4118aaa6d086cc768812823dea1
julia.buerk <julia.buerk@dhbw-engineering.de>
2024-05-08 15:27:14 +0200

    add utilities and const

---
+++ src/main/java/data/Utils.java
@@ -0,0 +1,16 @@
+package data;
+
+
+public class Utils {
+    public enum Company {
+        HERMES,
+        DHL
+    }
+
+    public enum Destination {
+        WORLD,
+        GERMANY,
+        EU
+    }
+}
+
